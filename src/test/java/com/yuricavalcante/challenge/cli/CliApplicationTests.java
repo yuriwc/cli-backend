6389dbff0c3ef2c3d0c2149ed7e64f04a44d61ec
Yuri Cavalcante <58712896+yuriwc@users.noreply.github.com>
2024-09-16 20:56:47 -0300

    chore: adicionar estrutura inicial do projeto Java/Spring
    Adicionar configuração básica do projeto, arquivos de configuração e estrutura de diretórios

---
+++ src/test/java/com/yuricavalcante/challenge/cli/CliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricavalcante.challenge.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
